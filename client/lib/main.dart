0ee854fbabffd409bd271421a829afcf4c9606b7
janith2021 <janithheshara1998@gmail.com>
2023-08-13 01:27:44 +0530

    Main.dart file modified

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -4,17 +4,7 @@ import 'package:client/providers/otp_provider.dart';
 import 'package:client/screen/Organization/organization_dashboard.dart';
 import 'package:client/screen/authentication/otp_screen.dart';
 import 'package:client/screen/authentication/password_change.dart';
-<<<<<<< HEAD
 import 'package:client/screen/organization/CreateCampaignForm.dart';
-=======
-
-import 'package:client/screen/organization/addCampaign.dart';
-
-
-import 'package:client/widget/splash_screen.dart';
-
-
->>>>>>> f72c4849582ecc2617fed50e9d2d16cc6c7040e1
 import 'package:client/screen/organization/createCampaign.dart';
 import 'package:client/screen/user/affectivity.dart';
 import 'package:client/screen/user/laboratoryReport.dart';
@@ -32,7 +22,7 @@ import 'package:splash_view/source/presentation/presentation.dart';
 import 'const/all_imports.dart';
 
 Future main() async {
-  // await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env");
   runApp(const Myapp());
 }
 
@@ -42,74 +32,46 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => LoginProvider()),
-        ChangeNotifierProvider(create: (_) => SignupProvider()),
-        ChangeNotifierProvider(create: (_) => ResetPasswordProvider()),
-        ChangeNotifierProvider(create: (_) => OtpProvider()),
-        ChangeNotifierProvider(create: (_) => ChangePasswordProvider()),
-        ChangeNotifierProvider(create: (_) => LaboratoryBookingProvider()),
-      ],
-      child: MaterialApp(
-
+        providers: [
+          ChangeNotifierProvider(create: (_) => LoginProvider()),
+          ChangeNotifierProvider(create: (_) => SignupProvider()),
+          ChangeNotifierProvider(create: (_) => ResetPasswordProvider()),
+          ChangeNotifierProvider(create: (_) => OtpProvider()),
+          ChangeNotifierProvider(create: (_) => ChangePasswordProvider()),
+          ChangeNotifierProvider(create: (_) => LaboratoryBookingProvider()),
+        ],
+        child: MaterialApp(
         debugShowCheckedModeBanner: false,
         scaffoldMessengerKey: Snackbar.scaffoldMessengerKey,
         // initialRoute: Splash.routename,
         // initialRoute: OtpScreen.routename,
         routes: {
-          Splash.routename : (_) => const Splash(),
-          Login.routeName  : (_)=>const Login(),
-          SignUp.routename : (_) => const SignUp(),
-          ResetPassword.routeName : (_) => const ResetPassword(),
-          OtpScreen.routename : (_) => const OtpScreen(),
-          ChangePassword.routename : (_) => const ChangePassword(),
-          UserDashboard.routename : (_) => const UserDashboard(),
-          GramaNiladhariDashBoard.routename : (_) => const GramaNiladhariDashBoard(),
-          PHIDashBoard.routename : (_) => const PHIDashBoard(),
+          Splash.routename: (_) => const Splash(),
+          Login.routeName: (_) => const Login(),
+          SignUp.routename: (_) => const SignUp(),
+          ResetPassword.routeName: (_) => const ResetPassword(),
+          OtpScreen.routename: (_) => const OtpScreen(),
+          ChangePassword.routename: (_) => const ChangePassword(),
+          UserDashboard.routename: (_) => const UserDashboard(),
+          GramaNiladhariDashBoard.routename: (_) =>
+              const GramaNiladhariDashBoard(),
+          PHIDashBoard.routename: (_) => const PHIDashBoard(),
+          OrganizationDashboard.routename: (_) => const OrganizationDashboard(),
+          CreateCampaign.routename: (_) => const CreateCampaign(),
+          Laboratory.routename: (_) => const Laboratory(),
+          LaboratoryBooking.routename: (_) => const LaboratoryBooking(),
+          NotificationPage.routename: (_) => const NotificationPage(),
+          Affectivity.routename: (_) => const Affectivity(),
+          LeaderBoard.routename: (_) => const LeaderBoard(),
+          MyForm.routename: (_) => MyForm(),
         },
-        home: SplashView(gradient: LinearGradient(colors: [AppColors.yellow,AppColors.orange]),loadingIndicator: CircularProgressIndicator(backgroundColor: AppColors.red,color: AppColors.green),logo: Image.asset("Assets/logo.png"),done: Done(const Login()),)
-      ),
-
-          debugShowCheckedModeBanner: false,
-          scaffoldMessengerKey: Snackbar.scaffoldMessengerKey,
-          // initialRoute: Splash.routename,
-          // initialRoute: OtpScreen.routename,
-          routes: {
-            Splash.routename: (_) => const Splash(),
-            Login.routeName: (_) => const Login(),
-            SignUp.routename: (_) => const SignUp(),
-            ResetPassword.routeName: (_) => const ResetPassword(),
-            OtpScreen.routename: (_) => const OtpScreen(),
-            ChangePassword.routename: (_) => const ChangePassword(),
-            UserDashboard.routename: (_) => const UserDashboard(),
-            GramaNiladhariDashBoard.routename: (_) =>
-                const GramaNiladhariDashBoard(),
-            PHIDashBoard.routename: (_) => const PHIDashBoard(),
-            OrganizationDashboard.routename: (_) =>
-                const OrganizationDashboard(),
-            CreateCampaign.routename: (_) =>  const CreateCampaign(),
-            Laboratory.routename : (_) => const Laboratory(),
-            LaboratoryBooking.routename : (_) => const LaboratoryBooking(),
-<<<<<<< HEAD
-            NotificationPage.routename : (_) => const NotificationPage(),
-            Affectivity.routename : (_) => const Affectivity(),
-            LeaderBoard.routename : (_) => const LeaderBoard(),
-            MyForm.routename : (_) =>  MyForm(),
-=======
-            CreateCampaign.routename: (_) => const CreateCampaign(),
-            MyForm.routename: (_) => MyForm(),
-
->>>>>>> f72c4849582ecc2617fed50e9d2d16cc6c7040e1
-          },
-          home: SplashView(
-            gradient:
-                LinearGradient(colors: [AppColors.yellow, AppColors.orange]),
-            loadingIndicator: CircularProgressIndicator(
-                backgroundColor: AppColors.red, color: AppColors.green),
-            logo: Image.asset("Assets/splash_screen/logo.png"),
-            done: Done(const Login()),
-          )),
-
-    );
+        home: SplashView(
+          gradient:
+              LinearGradient(colors: [AppColors.yellow, AppColors.orange]),
+          loadingIndicator: CircularProgressIndicator(
+              backgroundColor: AppColors.red, color: AppColors.green),
+          logo: Image.asset("Assets/splash_screen/logo.png"),
+          done: Done(const Login()),
+        )));
   }
 }
