3d7a2922375ecad31eabf7d09e6c6b73f31ee744
janith2021 <janithheshara1998@gmail.com>
2023-10-30 16:08:21 +0530

    Villager inform Affections and Symptoms page modified with some changes

--- client/lib/providers/symptomsinform.dart
+++ client/lib/providers/symptomsinform.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:client/const/all_imports.dart';
+import 'package:client/models/User.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class SymptomsinformProvider extends ChangeNotifier {
   TextEditingController controllercomments = TextEditingController();
@@ -7,11 +12,42 @@ class SymptomsinformProvider extends ChangeNotifier {
   String _symptomserror = "";
   String get symptomerror => _symptomserror;
 
-  symptomsinform() async {
+  symptomsinform(User item, BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    debugPrint(symptoms.toString());
     _symptomserror = ValidationHelper.validatesymptoms(symptoms);
-    debugPrint(_symptomserror);
+    // notifyListeners();
+    // debugPrint(_symptomserror.toString());
     if (_symptomserror == "") {
-      
+      var body = {
+        'name': item.name,
+        'houseHoldNo': prefs.getString('householdno'),
+        'divisionNumber': prefs.getString('divisionNumber'),
+        'nic': item.nic,
+        'symptoms': symptoms,
+        'contact': item.contact,
+      };
+      debugPrint(body.toString());
+      var fullurl = "${AllStrings.baseurl}/villager/informaffectivity";
+      setHeaders() =>
+          {'Content-Type': 'application/json', 'Accept': 'application/json'};
+      var res = await http.post(Uri.parse(fullurl),
+          body: jsonEncode(body), headers: setHeaders());
+      var result = await jsonDecode(res.body);
+      if (result['type'] == 'success') {
+        symptoms.clear();
+        Navigator.pop(context);
+        Navigator.pushReplacementNamed(context, "/villager/inform/affected2");
+
+        ArtSweetAlert.show(
+            context: context,
+            artDialogArgs: ArtDialogArgs(
+              title: "Success",
+              text: result['message'].toString(),
+              type: ArtSweetAlertType.success,
+            ));
+        return true;
+      }
     }
     notifyListeners();
   }
